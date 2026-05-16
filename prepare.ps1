ef47f3cb7fa305cc2f408e8eccf6776abff32044
Herasymenko Viktor <vg706638@gmail.com>
2026-05-16 22:21:48 +0300

    fix

--- prepare.ps1
+++ prepare.ps1
@@ -1,6 +1,8 @@
 $edgePath = "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
 $profileDir = "C:\playwright_edge_profile"
 
+New-Item -Path $profileDir -ItemType Directory -Force
+
 Write-Host "Starting Edge..." -ForegroundColor Cyan
 
 Stop-Process -Name "msedge" -ErrorAction SilentlyContinue
